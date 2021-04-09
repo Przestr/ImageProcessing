cf50c874bc60b8924e724b47d481d1ff10429cbf
PrzemyslawStruminski <struminski.przemyslaw@gmail.com>
2021-04-09 21:34:17 +0200

    Fixed threads management

--- src/com/struminski/imageprocessing/Main.java
+++ src/com/struminski/imageprocessing/Main.java
@@ -13,17 +13,23 @@ public class Main {
         image = imageManager.getImage();
 
         int start = 0;
-        int division = image.getHeight() / 4;
+        int threads = 4;
+        int division = image.getHeight() / threads;
         int end = division;
         ArrayList<Thread> threadList = new ArrayList<>();
+        long startTime = System.currentTimeMillis();
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < threads -1; i++) {
             Thread t = new Thread(new Context(new BinarizationAlgorithm(), image, start, end, image.getWidth()));
             t.start();
             threadList.add(t);
-            start = division;
+            start += division;
             end += division;
         }
+        Thread t = new Thread(new Context(new BinarizationAlgorithm(), image, start, image.getHeight(), image.getWidth()));
+        t.start();
+        threadList.add(t);
+
         try {
             for (Thread thread : threadList) {
                 thread.join();
@@ -31,7 +37,9 @@ public class Main {
         } catch (InterruptedException e) {
             System.out.println(e);
         }
-
+        long endTime = System.currentTimeMillis();
+        long duration = (endTime - startTime);
+        System.out.println("Execution time: " + duration + " miliseconds");
 
         imageManager.saveImage();
     }
